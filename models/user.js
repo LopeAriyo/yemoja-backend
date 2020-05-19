f2598fc13d2acdf57fa9980d79b2b70c23fecfe7
Lope Ariyo <lopeariyo@gmail.com>
2020-05-19 19:39:57 +0100

    Created user model

---
+++ models/user.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+const uniqueValidator = require("mongoose-unique-validator");
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    first_name: { type: String, required: true },
+    last_name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true, minLength: 6 },
+    estimated_cycle_length: { type: Number, required: true },
+    estimated_period_length: { type: Number, required: true },
+    cycles: { type: String, required: true },
+});
+
+userSchema.plugin(uniqueValidator);
+// module.exports = mongoose.model("Cycle", userSchema);
