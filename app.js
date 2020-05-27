3338fb4ceb61d17a20095c79376ae08e917487bd
Lope Ariyo <lopeariyo@gmail.com>
2020-05-27 23:19:50 +0100

    Added headers for access control

--- app.js
+++ app.js
@@ -13,6 +13,16 @@ const app = express();
 
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+    next();
+});
+
 app.use("/api/users", usersRoutes);
 app.use("/api/cycles", cyclesRoutes);
 
@@ -31,7 +41,7 @@ app.use((error, req, res, next) => {
 
 mongoose
     .connect(
-        "mongodb+srv://lope:1nPTxhYjPPgJVZep@cluster0-s7rff.mongodb.net/cycles?retryWrites=true&w=majority"
+        "mongodb+srv://lope:1nPTxhYjPPgJVZep@cluster0-s7rff.mongodb.net/mern?retryWrites=true&w=majority"
     )
     .then(() => {
         app.listen(5000);
